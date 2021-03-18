9808f07bd9c41a338c2989122cd94527d9187fbf
SMKarailiev <smkarailiev@gmail.com>
2021-03-18 08:53:22 +0200

    SECOND COMMIT

---
+++ CalculatingOfDays/ResultProject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatingOfDays
+{
+    public class ResultProject
+    {
+        // EmployeeID of first employee.
+        public int EmployeeID1 { get; set; }
+
+        // EmployeeID of second employee.
+        public int EmployeeID2 { get; set; }
+
+        // ProjectID of the project in which they worked together.
+        public List<int> ProjectIDS { get; set; }
+
+        // TotalDays that they worked together.
+        public int TotalDays { get; set; }
+
+        // Constructor
+        public ResultProject()
+        {
+            ProjectIDS = new List<int>();
+        }
+    }
+}
